e9d5f25ba10365cb9bcb4f549a8fb1a7a407e800
FractalFox <flgtom@aol.com>
2018-05-09 17:34:57 -0700

    Part of learnyoumongo's Update challenge.

---
+++ Javascript/mongodbExample/mongoPromiseError.js
@@ -0,0 +1,23 @@
+function example(req, res){
+  // Queries the database for all records then displays contents in web page.
+  mongo.connect(url)
+    .then(function(db){
+      let dbo = db.db('your db here');
+      dbo.collection('your collection here').find({}).toArray()
+        .then(function(resp){
+          res.status(200).set({'content-type':'text/html'});
+          res.write("<h1>" + JSON.stringify(resp) + "</h1>");
+          res.end();
+        })// I think this one is needed as well, idk.
+        .catch(function(err){
+          console.log(err.stack);
+        })
+    })// The error is because these .catches are missing when the promise returns reject
+    .catch(function(err){
+      console.log(err.stack);
+    });
+}
+
+app.get('/*', (req, res) => {
+  example();
+});
