ccb8fa63f83c9427f2534bcb4ac67dbbccfc6b22
Tom_Vandivier <flgtom@aol.com>
2018-07-04 09:32:09 -0700

    Added dollarSignFunction

---
+++ Javascript/examples/dollarSignFunction.js
@@ -0,0 +1,11 @@
+// Copied from https://osric.com/chris/accidental-developer/2008/04/the-javascript-dollar-sign-function/
+function $(element) {
+  if (arguments.length > 1) {
+    for (var i = 0, elements = [], length = arguments.length; i < length; i++)
+      elements.push($(arguments[i]));
+    return elements;
+  }
+  if (Object.isString(element))
+    element = document.getElementById(element);
+  return Element.extend(element);
+}
