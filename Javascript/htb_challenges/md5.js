6abd4fcc84d21eea7f47ac43fe98eb2b83fae6fb
Tom_Vandivier <fractalfox5@gmail.com>
2020-01-03 20:28:09 -0800

    Update md5.js

--- Javascript/htb_challenges/md5.js
+++ Javascript/htb_challenges/md5.js
@@ -1,13 +1,13 @@
-/************************************************************************************
-*  AUTHOR: Thomas V.
-*  CREATED: 01/03/2020, 20:20:20
-*  A Small script for hackthebox.eu's web challenge Emdee
-*
-*  This script queries a URL hosted by hackthebox.eu for a string in an html page.
-*  It then hashes the string and passes the string and a cookie to a second function
-*  that makes a request back to the URL with the string hased using the md5 hashing
-*  alogrithm.
-*************************************************************************************/
+/***************************************************************************************
+**  AUTHOR: Thomas V.                                                                 **
+**  CREATED: 01/03/2020, 20:20:20                                                     **
+**  A Small script for hackthebox.eu's web challenge Emdee                            **
+**                                                                                    **
+**  This script queries a URL hosted by hackthebox.eu for a string in an html page.   **
+**  It then hashes the string and passes the string and a cookie to a second function **
+**  that makes a request back to the URL with the string hased using the md5 hashing  **
+**  alogrithm.                                                                        **
+****************************************************************************************/
 
 const http = require('http');
 var querystring = require('querystring');
